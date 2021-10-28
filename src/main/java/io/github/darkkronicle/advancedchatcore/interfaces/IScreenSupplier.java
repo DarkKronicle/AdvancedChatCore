2f1d3a6177d478b35570edc76843bb2226dd0cc6
DarkKronicle <darkkronicle@gmail.com>
2021-10-27 19:29:15 -0600

    Adding license headers

--- src/main/java/io/github/darkkronicle/advancedchatcore/interfaces/IScreenSupplier.java
+++ src/main/java/io/github/darkkronicle/advancedchatcore/interfaces/IScreenSupplier.java
@@ -1,3 +1,12 @@
+/*
+ * Mozilla Public License v2.0
+ *
+ * Copyright (C) 2021 DarkKronicle
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 package io.github.darkkronicle.advancedchatcore.interfaces;
 
 import java.util.function.Supplier;
