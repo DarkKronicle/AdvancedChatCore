1b5f34692b7f7d6f3281a6497b7f53f926617c6e
DarkKronicle <darkkronicle@gmail.com>
2021-10-29 14:37:43 -0600

    Making building possible with jitpack

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-./gradlew spotlessApply
+./gradlew spotlessApply --add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
+  --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
+  --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED \
+  --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
+  --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED
 # Restage
 git update-index --again
