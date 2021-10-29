1b96fab17bbde0e057dda6ab8818e15dedbf378a
DarkKronicle <darkkronicle@gmail.com>
2021-10-29 14:42:17 -0600

    Hopefully arguments work

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-./gradlew spotlessApply --add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
-  --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
-  --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED \
-  --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
-  --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED
+./gradlew spotlessApply --add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-exports =jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED-add-exports= jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED
 # Restage
 git update-index --again
