7c5a2686e49bc309ef6d37b34610d1f192159aa4
DarkKronicle <darkkronicle@gmail.com>
2022-04-14 15:19:20 -0600

    Allow opening of chat screen in nether portals

---
+++ src/main/java/io/github/darkkronicle/advancedchatcore/mixin/MixinClientPlayerEntity.java
@@ -0,0 +1,45 @@
+package io.github.darkkronicle.advancedchatcore.mixin;
+
+import io.github.darkkronicle.advancedchatcore.chat.AdvancedChatScreen;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerEntity.class)
+public abstract class MixinClientPlayerEntity extends Entity {
+
+    @Shadow @Final protected MinecraftClient client;
+
+    @Shadow public float nextNauseaStrength;
+
+    public MixinClientPlayerEntity(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(
+            method="updateNausea",
+            at = @At(value="INVOKE", target="Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V"),
+            cancellable = true
+    )
+    public void updateNauseaHook(CallbackInfo ci) {
+        if (client.currentScreen instanceof AdvancedChatScreen) {
+            ci.cancel();
+            nextNauseaStrength += 0.0125f;
+            if (this.nextNauseaStrength >= 1.0f) {
+                this.nextNauseaStrength = 1.0f;
+            }
+            inNetherPortal = false;
+        }
+    }
+
+
+}
