4c7ccd1b2e83ba56ac03e712d4561f6f0d07bf01
Mysticdrew <drewhaas@gmail.com>
2022-04-23 16:44:20 -0500

    fix mod conflicts with chat mixins

--- src/main/java/io/github/darkkronicle/advancedchatcore/mixin/MixinSleepingChatScreen.java
+++ src/main/java/io/github/darkkronicle/advancedchatcore/mixin/MixinSleepingChatScreen.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 DarkKronicle
+ * Copyright (C) 2021-2022 DarkKronicle
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -7,24 +7,30 @@
  */
 package io.github.darkkronicle.advancedchatcore.mixin;
 
-import fi.dy.masa.malilib.gui.GuiBase;
-import io.github.darkkronicle.advancedchatcore.chat.AdvancedSleepingChatScreen;
+import io.github.darkkronicle.advancedchatcore.chat.AdvancedChatScreen;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.SleepingChatScreen;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Environment(EnvType.CLIENT)
 @Mixin(SleepingChatScreen.class)
-public class MixinSleepingChatScreen {
+public class MixinSleepingChatScreen extends ChatScreen {
 
-    @Inject(method = "init", at = @At("HEAD"), cancellable = true)
-    public void sleepInit(CallbackInfo ci) {
-        // Open the Advanced sleeping screen
-        GuiBase.openGui(new AdvancedSleepingChatScreen());
-        ci.cancel();
+    public MixinSleepingChatScreen() {
+        super("");
+    }
+
+    @ModifyArg(method = "closeChatIfEmpty",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V", ordinal = 1))
+    public Screen openSleepingChatScreen(@Nullable Screen screen) {
+
+        return new AdvancedChatScreen(this.chatField.getText());
     }
 }
